1d9e7acbd3e256b9d29792e5e21b68f6d62f3989
Orri Axelsson <orriaxels@gmail.com>
2017-12-04 13:50:43 +0000

    fix

---
+++ MovieSearch/MovieDownload/ImageDownloader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MovieSearch.Model;
+
+
+namespace MovieDownload
+{
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class ImageDownloader
+    {
+        private IImageStorage _imageStorage;
+
+        public ImageDownloader(IImageStorage imageStorage)
+        {
+            this._imageStorage = imageStorage;
+        }
+
+        public string LocalPathForFilename(string remoteFilePath)
+        {
+            if (remoteFilePath == null)
+            {
+                return string.Empty;
+            }
+
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string localPath = Path.Combine(documentsPath, remoteFilePath.TrimStart('/'));
+            return localPath;
+        }
+
+        public async Task DownloadImage(string remoteFilePath, string localFilePath, CancellationToken token)
+        {
+            var fileStream = new FileStream(
+                                 localFilePath,
+                                 FileMode.Create,
+                                 FileAccess.Write,
+                                 FileShare.None,
+                                 short.MaxValue,
+                                 true);
+            try
+            {
+                await this._imageStorage.DownloadAsync(remoteFilePath, fileStream, token);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public async Task DownloadImagesInList(List<MovieDetails> movieDisplayInfoList)
+        {
+            foreach (var mInfo in movieDisplayInfoList)
+            {
+                var posterPath = mInfo.imageUrl;
+                var localPosterPath = this.LocalPathForFilename(posterPath);
+                mInfo.posterFilePath = localPosterPath;
+                if (localPosterPath != string.Empty && !File.Exists(localPosterPath))
+                {
+                    await this.DownloadImage(posterPath, localPosterPath, new CancellationToken());
+                }
+            }
+        }
+    }
+}
---
+++ MovieSearch/MovieSearch/Service/MovieService.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using DM.MovieApi;
+using DM.MovieApi.MovieDb.Movies;
+using DM.MovieApi.ApiResponse;
+using System.Threading.Tasks;
+using MovieSearch.Model;
+
+namespace MovieSearch.Service
+{
+    public class MovieService
+    {
+        public string apiKey = "807f816252d83b681cf3b2efe5ffe5b0";
+        public string apiUrl = "https://api.themoviedb.org/3/";
+
+        private IApiMovieRequest _api;
+        private List<MovieDetails> _movies;
+
+        public MovieService()
+        {
+            MovieDbFactory.RegisterSettings(apiKey, apiUrl);
+            _api = MovieDbFactory.Create<IApiMovieRequest>().Value;
+            _movies = new List<MovieDetails>();
+        }
+
+        public async Task<List<MovieDetails>> GetMovieByTitle(string title)
+        {
+            ApiSearchResponse<MovieInfo> response = await _api.SearchByTitleAsync(title);
+
+            if(_movies == null)
+            {
+                _movies.Clear();
+            }
+            else
+            {
+                _movies = new List<MovieDetails>();    
+            }
+
+            foreach (MovieInfo info in response.Results)
+            {
+                _movies.Add(new MovieDetails
+                {
+                    id = info.Id,
+                    title = info.Title,
+                    imageUrl = info.PosterPath,
+                    releaseDate = info.ReleaseDate,
+                    voteAverage = info.VoteAverage,
+                    voteCount = info.VoteCount,
+                    posterFilePath = "",
+                    runtime = "",
+                    genres = new List<String>(),
+                    actors = new List<String>()
+                });
+            }
+
+            return _movies;
+        }
+
+        public async Task<List<String>> GetCreditList(int id)
+        {
+            ApiQueryResponse<MovieCredit> cast = await _api.GetCreditsAsync(id);
+
+            List<String> actors = new List<String>();
+
+            for (int i = 0; i < cast.Item.CastMembers.Count && i < 3; i++)
+            {
+                actors.Add(cast.Item.CastMembers[i].Name);
+            }    
+
+            return actors;
+        }
+
+        public async Task<MovieDetails> GetMovieDetail(int id)
+        {
+            ApiQueryResponse<Movie> movieInfo = await _api.FindByIdAsync(id);
+
+            MovieDetails movie = new MovieDetails()
+            {
+                title = movieInfo.Item.Title,
+                runtime = movieInfo.Item.Runtime.ToString(),
+                description = movieInfo.Item.Overview,
+                tagLine = movieInfo.Item.Tagline,
+                budget = movieInfo.Item.Budget,
+                genres = new List<String>()
+            };
+
+            for (int i = 0; i < movieInfo.Item.Genres.Count; i++)
+            {
+                movie.genres.Add(movieInfo.Item.Genres[i].ToString());
+            }
+
+            return movie;
+        }
+
+        public List<MovieDetails> GetMovies()
+        {
+            return _movies;
+        }
+
+    }
+}
